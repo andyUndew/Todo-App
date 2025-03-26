2bfbdff0841eb2a0c95f33bb8bce9c25a25be2f1
andyUndew <undew.info@gmail.com>
2025-03-26 16:27:59 +0900

    fd

--- src/SideMenu.tsx
+++ src/SideMenu.tsx
@@ -178,7 +178,7 @@ export default function SideMenu({
                     <CaretDownIcon className="CaretDown" aria-hidden />
                   </Button>
                 </NavigationMenu.Trigger>
-                <NavigationMenu.Content className="NavigationMenuContent">
+                <NavigationMenu.Content className="NavigationMenuContent" forceMount>
                   <Flex p="4" direction="column" align="stretch">
                     {notes.map((note) => (
                       <Button
