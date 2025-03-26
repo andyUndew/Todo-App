e9955c087f2ba3ff7cac6ae44c2ab41ad4ff8c79
andyUndew <undew.info@gmail.com>
2025-03-26 17:31:55 +0900

    てて

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import babel from "vite-plugin-babel";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({
-      babelConfig: {
-        plugins: ["@babel/plugin-transform-named-capturing-groups-regex"],
-      },
-    }),
-  ],
+  plugins: [react()],
+  build: {
+    target: "es2017",
+  },
 });
